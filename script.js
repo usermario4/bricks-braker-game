65c36ce99b2bd6b9a95338c0bbf4954b040d2084
CadarSebastian <sebicadar03@gmail.com>
2023-03-17 11:41:48 +0200

    comitus

--- script.js
+++ script.js
@@ -3,11 +3,34 @@ function setup(){
 }
 
 function draw(){
-    background("#ff0000")
+    background("#00F6FE")
 
     smileyFacerSebi();
 }
 
 function  smileyFacerSebi(){
+    //skin   
+    fill("#fff000");
+    
     circle(200,200, 100)
+     //mouth 
+//arc(200,210,20,-10,0,Math.PI)
+    
+    fill("#ff0000")
+    circle(225,180,30)
+    circle(175,180,30)
+//left eye
+    fill("#000000") 
+    circle(225,180,10)
+line(230, 200, 170,200)
+    //right eye
+    circle(175,180,10)
+    fill("#ffffff")
+    line(227,210,230,200)
+    
+    
+   
+
+   
+   
 }
