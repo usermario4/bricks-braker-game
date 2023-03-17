3b964134ec57a7002b8983b67ccb3534eec49566
CadarSebastian <sebicadar03@gmail.com>
2023-03-17 10:41:37 +0200

    comitus

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function setup(){
-    createCanvas(400,500)
+    createCanvas(400,400)
 }
 
 function draw(){
     background("#ff0000")
 
+    smileyFacerSebi();
+}
+
+function  smileyFacerSebi(){
+    circle(200,200, 100)
 }
