e0ae03ecc14ed4337fa5006195db7bff5a46e014
CadarSebastian <126519667+CadarSebastian@users.noreply.github.com>
2023-03-17 11:54:43 +0200

    Smiley facer (#2)
    * comitus
    
    * comitus

--- script.js
+++ script.js
@@ -3,10 +3,33 @@ function setup(){
 }
 
 function draw(){
-    background("#00ff00")
 
-smileyFaceMario();
+    background("#00F6FE")
 
+    smileyFacerSebi();
+    smileyFaceMario();
+
+}
+
+function  smileyFacerSebi(){
+    //skin   
+    fill("#fff000");
+    
+    circle(200,200, 100)
+     //mouth 
+//arc(200,210,20,-10,0,Math.PI)
+    
+    fill("#ff0000")
+    circle(225,180,30)
+    circle(175,180,30)
+//left eye
+    fill("#000000") 
+    circle(225,180,10)
+line(230, 200, 170,200)
+    //right eye
+    circle(175,180,10)
+    fill("#ffffff")
+    line(227,210,230,200)
 }
 
 function smileyFaceMario(){
