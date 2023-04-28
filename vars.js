7fd33504ff936ecb4365d693db325bd9c592cad9
usermario4 <126523180+usermario4@users.noreply.github.com>
2023-04-28 08:52:09 +0300

    Smiley facer (#4)
    * comitus
    
    * comitus
    
    * comitus
    
    * comitus
    
    * comitus
    
    * comitus
    
    ---------
    
    Co-authored-by: CadarSebastian <sebicadar03@gmail.com>
    Co-authored-by: CadarSebastian <126519667+CadarSebastian@users.noreply.github.com>

---
+++ vars.js
@@ -0,0 +1,22 @@
+let moveS1X = 300;
+let moveS1Y = 200;
+
+let moveS2X = 300;
+let moveS2Y = 200;
+
+let sensS1X = 1;
+let sensS1Y = 1;
+
+let sensS2X = 1;
+let sensS2Y = 1;
+
+let speedS1=3;//viteza
+ rayS1=60;
+
+ let speedS2=6;//viteza
+ rayS2=60;
+ 
+let limitTop = 0;
+let limitBottom = 600;
+let limitLeft = 0;
+let limitRight = 600;
