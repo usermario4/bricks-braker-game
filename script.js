92bf0d4ae6288674b047a44c2193b8650ae8dcdb
CadarSebastian <sebicadar03@gmail.com>
2023-04-28 09:04:59 +0300

    comitus

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function setup() {
 function draw(){
     background("#00F6FE")
 
-sensS2X = sensSmileFaceCheckX(moveS2X, rayS2, sensS2X);
-sensS2Y = sensSmileFaceCheckX(moveS2Y, rayS2, sensS2Y);
+smileyFacerSebi.X = sensSmileFaceCheckX(smileyFacerSebi.x, smileyFacerSebi.ray, smileyFacerSebi.X);
+smileyFacerSebi.Y = sensSmileFaceCheckX(smileyFacerSebi.y, smileyFacerSebi.ray, smileyFacerSebi.Y);
 
 sensS1X = sensSmileFaceCheckX(moveS1X, rayS1, sensS1X);
 sensS1Y = sensSmileFaceCheckX(moveS1Y, rayS1, sensS1Y);
@@ -14,11 +14,11 @@ sensS1Y = sensSmileFaceCheckX(moveS1Y, rayS1, sensS1Y);
   moveS1X += sensS1X*speedS1;
   moveS1Y += sensS1Y*speedS1;
 
-  smileyFacerSebi.X += smileyFacerSebi.X*smileyFacerSebi.speed;
-  smileyFacerSebi.Y += smileyFacerSebi.Y*smileyFacerSebi.speed;
+  smileyFacerSebi.x += smileyFacerSebi.sensX*smileyFacerSebi.speed;
+  smileyFacerSebi.y += smileyFacerSebi.sensY*smileyFacerSebi.speed;
 
 smileyFaceMario(moveS1X,moveS1Y,rayS1*2);
-smileyFacerSebi(moveS2X,moveS2Y,rayS2*2);
+smileyFacerSebi(smileyFacerSebi.X,smileyFacerSebi.Y,smileyFacerSebi.ray*2);
 
 
 
