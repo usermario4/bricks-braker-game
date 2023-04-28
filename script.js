11582abcc5cadc6bb7adab019a990dd710e86fe5
CadarSebastian <sebicadar03@gmail.com>
2023-04-28 08:55:57 +0300

    comitus

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function setup() {
 function draw(){
     background("#00F6FE")
 
-  sensS2X = sensSmileFaceCheckX(moveS2X, rayS2, sensS2X);
+sensS2X = sensSmileFaceCheckX(moveS2X, rayS2, sensS2X);
 sensS2Y = sensSmileFaceCheckX(moveS2Y, rayS2, sensS2Y);
 
 sensS1X = sensSmileFaceCheckX(moveS1X, rayS1, sensS1X);
@@ -14,8 +14,8 @@ sensS1Y = sensSmileFaceCheckX(moveS1Y, rayS1, sensS1Y);
   moveS1X += sensS1X*speedS1;
   moveS1Y += sensS1Y*speedS1;
 
-  moveS2X += sensS2X*speedS2;
-  moveS2Y += sensS2Y*speedS2;
+  smileyFacerSebi.X += smileyFacerSebi.X*smileyFacerSebi.speed;
+  smileyFacerSebi.Y += smileyFacerSebi.Y*smileyFacerSebi.speed;
 
 smileyFaceMario(moveS1X,moveS1Y,rayS1*2);
 smileyFacerSebi(moveS2X,moveS2Y,rayS2*2);
